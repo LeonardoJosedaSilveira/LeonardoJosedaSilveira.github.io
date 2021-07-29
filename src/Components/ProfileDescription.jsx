743601f405b408c26a0b231f0a5c7c6a6ae9eb19
Leonardo José da Silveira <rpgleo.js@gmail.com>
2021-07-29 09:11:47 -0300

    add first description

--- src/Components/ProfileDescription.jsx
+++ src/Components/ProfileDescription.jsx
@@ -1,7 +1,8 @@
+const describe = 'I am very calm, I practice Shotokan Karate, I like games, technology, I roller-skate, I fish. I am learning Full Stack Web Development by TRYBE but I have that appreciation for the Back End.'
 function ProfileDescription() {
   return (
     <div>
-      <p>texto de teste para ser alterado depois</p>
+      <p>{ describe }</p>
     </div>
   )
 }
