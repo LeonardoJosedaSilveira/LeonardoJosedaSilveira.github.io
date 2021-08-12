89421e168a5680bfe5d7ec0ff578e7f293a6e30d
Leonardo José da Silveira <rpgleo.js@gmail.com>
2021-08-12 01:12:22 -0300

    edit text

--- src/Data/Description.jsx
+++ src/Data/Description.jsx
@@ -1,3 +1,3 @@
-const Description = 'I am very calm, I practice Shotokan Karate, I like games, technology, I rollerblade, I fish. I am learning Full Stack Web Development by TRYBE but I have that appreciation for the Back-End.'
+const Description = 'I am very calm, I practice Shotokan Karate, like games, technology, rollerblade and fishing. I am learning Full Stack Web Development by TRYBE but I have that appreciation for the Back-End.'
 
 export default Description
